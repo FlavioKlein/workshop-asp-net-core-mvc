0f936a430962e531dfd7ad8ae10f89ebddb34529
Flavio Klein <flavioklein@gmail.com>
2019-02-16 20:55:11 -0200

    Delete Seller

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -17,9 +17,22 @@ namespace SalesWebMVC.Services
         {
             return _context.Seller.OrderBy(ord => ord.Name).ToList();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);            
+        }
+
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }

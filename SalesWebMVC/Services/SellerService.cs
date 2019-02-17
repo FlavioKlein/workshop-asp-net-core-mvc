e6516cb6bbe0b2192efe8e65f8e8f021b26a8734
Flavio Klein <flavioklein@gmail.com>
2019-02-17 12:35:53 -0300

    Gerenciando exception em deleção e garantindo integridade referencial

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Services.Exceptions;
 using SalesWebMVC.Models;
+using System;
 
 namespace SalesWebMVC.Services
 {
@@ -35,9 +36,16 @@ namespace SalesWebMVC.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales.");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
